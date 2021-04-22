eff33d85326c8a279e56857056e2939eb0502659
Mohammad Ali <ali.shiekh16@outlook.com>
2021-04-22 16:09:22 +0500

    Refactored block plan.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+const collapsibles = document.querySelectorAll('.collapsible');
+console.log(collapsibles);
+collapsibles.forEach(item =>
+  item.addEventListener('click', function () {
+    this.classList.toggle('collapsible--expanded');
   })
 );
